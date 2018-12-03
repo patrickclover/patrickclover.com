b6d5d08b9469f203adfe03536feaf74f9290e726
patrickclover <patrick@blackbx.io>
2018-12-03 18:48:03 +0000

    dates

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,51 +1,54 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import './Blog.scss'
-import Title from '../Title/Title'
+import Title                from '../Title/Title'
 
 const blogUrl = 'https://medium.com/feed/@patrickclover'
 const webBlog = 'https://blackbx.io/blog/feed/'
 
 class Blog extends Component {
 
-    constructor() {
+    constructor () {
         super()
         this.state = {
-            posts: []
+            posts: [],
         }
     }
 
-    componentDidMount() {
+    componentDidMount () {
         //this.getPosts(blogUrl)
         this.getPosts(webBlog)
     }
 
-    getPosts(uri) {
+    getPosts (uri) {
         const url = new URL('https://api.rss2json.com/v1/api.json')
         url.searchParams.append('rss_url', uri)
         fetch(url)
-            .then(response => response.json())
-            .then(response => {
-                if (response.status !== 'ok') return
-                return this.setState({posts: response.items})
-            })
-            .catch(error => console.log(error))
+        .then(response => response.json())
+        .then(response => {
+            if (response.status !== 'ok') return
+            return this.setState({posts: response.items})
+        })
+        .catch(error => console.log(error))
     }
 
-    formatDate(date) {
-        const d = new Date(date)
+    formatDate (date) {
+
+        if (!date) return
+        const d = new Date(date.toString())
+        if (d.toString() === 'Invalid Date') return
+        console.log(d,date.toString())
         return new Intl.DateTimeFormat('en-GB', {
             year: 'numeric',
             month: 'short',
-            day: '2-digit'
+            day: '2-digit',
         }).format(d)
     }
 
-    render() {
+    render () {
         const items = this.state.posts
         return (
             <div className="blog hidden-sm">
                 <Title heading="On the line"/>
-
                 {
                     items.map((item, k) =>
                         <div key={k} className="__item">
@@ -53,7 +56,7 @@ class Blog extends Component {
                                 <a target="_blank" href={item.link}>{item.title}</a>
                                 <span>{this.formatDate(item.pubDate)}</span>
                             </h2>
-                        </div>
+                        </div>,
                     )
                 }
             </div>
